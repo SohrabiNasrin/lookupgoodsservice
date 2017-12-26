e3b6d5afad770a2b054f13f5ad7f5c0dd5c893b3
= <=>
2017-12-26 11:17:13 +1100

    initial  commit

---
+++ src/main/java/com/nasrinsohrabi/lookupgoods/LookupgoodsApplication.java
@@ -0,0 +1,12 @@
+package com.nasrinsohrabi.lookupgoods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LookupgoodsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LookupgoodsApplication.class, args);
+	}
+}
